d8bf040158827a362d77b700539c8d1ae89b7356
Abdulkarim Ghazi <abdulkarim-ghazi@users.noreply.github.com>
2026-06-03 22:15:52 +0200

    Fix route ordering and brand panel

--- routes/web.php
+++ routes/web.php
@@ -21,19 +21,25 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->n
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/handbook', [HandbookController::class, 'show'])->name('handbook');
-    Route::get('/modules/{module}', [RecordModuleController::class, 'index'])->name('modules.index');
     Route::get('/modules/{module}/records/{record}/edit', [RecordModuleController::class, 'edit'])->name('modules.edit');
-    Route::resource('records', DesignerRecordController::class)->only(['index', 'show']);
-    Route::get('/documents/{document}', [DocumentController::class, 'show'])->name('documents.show');
+    Route::get('/modules/{module}', [RecordModuleController::class, 'index'])->name('modules.index');
+    Route::get('/records', [DesignerRecordController::class, 'index'])->name('records.index');
 
     Route::middleware('design_manager')->group(function () {
         Route::get('/designers/create', [DesignerRecordController::class, 'createDesigner'])->name('designers.create');
         Route::post('/designers', [DesignerRecordController::class, 'storeDesigner'])->name('designers.store');
+        Route::get('/records/create', [DesignerRecordController::class, 'create'])->name('records.create');
+        Route::post('/records', [DesignerRecordController::class, 'store'])->name('records.store');
+        Route::get('/records/{record}/edit', [DesignerRecordController::class, 'edit'])->name('records.edit');
+        Route::put('/records/{record}', [DesignerRecordController::class, 'update'])->name('records.update');
+        Route::delete('/records/{record}', [DesignerRecordController::class, 'destroy'])->name('records.destroy');
         Route::put('/modules/{module}/records/{record}', [RecordModuleController::class, 'update'])->name('modules.update');
-        Route::resource('records', DesignerRecordController::class)->except(['index', 'show']);
         Route::post('/records/{record}/documents', [DocumentController::class, 'store'])->name('records.documents.store');
         Route::delete('/documents/{document}', [DocumentController::class, 'destroy'])->name('documents.destroy');
     });
+
+    Route::get('/records/{record}', [DesignerRecordController::class, 'show'])->name('records.show');
+    Route::get('/documents/{document}', [DocumentController::class, 'show'])->name('documents.show');
 
     Route::middleware('admin')->group(function () {
         Route::resource('users', UserController::class)->except(['show']);
